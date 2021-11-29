a595dace378d5ce4f5973cc2730425eb8bb0b94c
rus733 <9282716617@mail.ru>
2021-11-29 16:29:43 +0300

    сохранение перед  переходом в dev

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-let title, screens, screeenPrice, rollback, fullPrice, adaptive; // в столбик форматер не позволяет
+let title = "proJS22";
+let screens = "Простые, Сложные, Интерактивные";
+let screeenPrice = 1000;
+let rollback = 10;
+let fullPrice = 10000;
+let adaptive = true;
 
-alert("Привет всем!!!");
-console.log("Привет , это консоль из lesson01!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log("строка имеет длину, равную " + screens.length + " символ");
+console.log(
+  "Стоимость верстки экранов " +
+    screeenPrice +
+    " юаней \nСтоимость разработки сайта " +
+    fullPrice +
+    " юаней"
+);
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log(fullPrice * (rollback / 100));
+
+console.log(`Стоимость верстки экранов ${screeenPrice} юаней
+Стоимость разработки сайта ${fullPrice} юаней`);
+
+//alert("Привет всем!!!");
+//console.log("Привет , это консоль из lesson02!");
