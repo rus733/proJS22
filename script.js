c5589e27b179a303f05642d3e03936bee5bde005
rus733 <9282716617@mail.ru>
2021-11-30 21:23:14 +0300

    задание подготовлено к проверке

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 "use strict";
 // урок 4
+
+let sliced, str;
+
+let title = prompt(" введите строку");
+
+const myFunc = function (str) {
+  if (typeof str !== "string") {
+    console.log(" Не строка");
+  } else {
+    str = str.trim();
+    sliced = str.slice(0, 30);
+    if (sliced.length < str.length) {
+      console.log((sliced += "..."));
+      return (sliced += "...");
+    }
+  }
+};
+myFunc(title);
