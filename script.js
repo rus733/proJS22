6d2fc3c4c1c4debd1cad095b5f661e457fdff0be
rus733 <9282716617@mail.ru>
2021-11-30 21:46:07 +0300

    задание подготовлено к проверке

--- script.js
+++ script.js
@@ -1,20 +1,61 @@
 "use strict";
 // урок 4
+let title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?");
+const screenPrice = +prompt("Сколько будет стоить данная работа?");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+const rollback = 10;
+let fullPrice;
+let allServicePrices;
+let servicePercentPrice;
 
-let sliced, str;
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
 
-let title = prompt(" введите строку");
-
-const myFunc = function (str) {
-  if (typeof str !== "string") {
-    console.log(" Не строка");
+const getRollbackMessage = function (price) {
+  if (price >= 30000) {
+    return "Даем скидку в 10%";
+  } else if (15000 <= price && price < 30000) {
+    return "Даем скидку в 5%";
+  } else if (0 <= price && price < 15000) {
+    return "Скидка не предусмотрена";
   } else {
-    str = str.trim();
-    sliced = str.slice(0, 30);
-    if (sliced.length < str.length) {
-      console.log((sliced += "..."));
-      return (sliced += "...");
-    }
+    return "Что то пошло не так";
   }
 };
-myFunc(title);
+
+const getAllServicePrices = function () {
+  return servicePrice1 + servicePrice2;
+};
+allServicePrices = getAllServicePrices();
+
+const getFullPrice = function () {
+  return screenPrice + allServicePrices;
+};
+fullPrice = getFullPrice();
+
+let getTitle = function (string) {
+  string = string.trim();
+  return string[0].toUpperCase() + string.substr(1).toLowerCase();
+};
+getTitle(title);
+console.log(getTitle(title));
+
+const getServicePercentPrices = function () {
+  return Math.ceil(fullPrice * (1 - rollback / 100));
+};
+servicePercentPrice = getServicePercentPrices();
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(screens.split(""));
+console.log(screens);
+console.log(getRollbackMessage(fullPrice));
+console.log(servicePercentPrice);
