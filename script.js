7aafdc6e474ebdc1efb454d0f39396cad56636ef
rus733 <9282716617@mail.ru>
2021-12-01 21:21:44 +0300

    внесли код из практики в  lesson05

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 "use strict";
-// lesson04 принято- делаем новую ветку под lesson05
-const title = prompt("Как называется ваш проект?");
-const screens = prompt("Какие типы экранов нужно разработать?");
-const screenPrice = +prompt("Сколько будет стоить данная работа?");
-const adaptive = confirm("Нужен ли адаптив на сайте?");
-const service1 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice1 = +prompt("Сколько это будет стоить?");
-const service2 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice2 = +prompt("Сколько это будет стоить?");
+
+// lesson05 из практики
+
+let title;
+let screens;
+let screenPrice;
+let adaptive;
 const rollback = 10;
+let service1;
+let service2;
+//const servicePrice1 = +prompt("Сколько это будет стоить?");
+//const servicePrice2 = +prompt("Сколько это будет стоить?");
+
+//console.log(!isNaN(parseFloat(num)) && isFinite()); // проверка на число
+
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+const asking = function () {
+  title = prompt("Как называется ваш проект?", "Калькуляторверстки");
+  screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные");
+  screenPrice = prompt("Сколько будет стоить данная работа?");
+  while (!isNumber(screenPrice)) {
+    screenPrice = prompt("Сколько будет стоить данная работа?");
+  }
+  adaptive = confirm("Нужен ли адаптив на сайте?");
+};
 
 const showTypeOf = function (variable) {
   console.log(variable, typeof variable);
@@ -27,14 +45,25 @@ const getRollbackMessage = function (price) {
 };
 
 const getAllServicePrices = function () {
-  return servicePrice1 + servicePrice2;
+  let sum = 0;
+
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      service1 = prompt("Какой дополнительный тип услуги нужен?");
+    } else if (i === 1) {
+      service2 = prompt("Какой дополнительный тип услуги нужен?");
+    }
+
+    sum += +prompt("Сколько это будет стоить?");
+  }
+
+  return sum;
+  //return servicePrice1 + servicePrice2;
 };
-const allServicePrices = getAllServicePrices();
 
 const getFullPrice = function () {
   return screenPrice + allServicePrices;
 };
-const fullPrice = getFullPrice();
 
 const getTitle = function (string) {
   const internVar = string.trim().toLowerCase();
@@ -44,7 +73,12 @@ const getTitle = function (string) {
 const getServicePercentPrices = function () {
   return Math.ceil(fullPrice * (1 - rollback / 100));
 };
+
+asking();
+const allServicePrices = getAllServicePrices();
+const fullPrice = getFullPrice();
 const servicePercentPrice = getServicePercentPrices();
+//title = getTitle();
 
 const screensSpilt = screens.toLowerCase().split();
 
@@ -52,8 +86,16 @@ showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
-console.log(getTitle(title));
+console.log("allServicePrices", allServicePrices);
+
+console.log(getTitle(title)); //
 console.log(screensSpilt);
 console.log(screens);
 console.log(getRollbackMessage(fullPrice));
 console.log(servicePercentPrice);
+
+//while (
+//isNaN(screenPrice) ||
+//screenPrice.trim() === "" ||
+//screenPrice === null
+//)   // не очень удачная проверка на число
--- temp.js
+++ temp.js
@@ -1,5 +1,66 @@
 "use strict";
-// 4 урок темп
+
+// lesson04 принято- делаем новую ветку под lesson05
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?");
+const screenPrice = +prompt("Сколько будет стоить данная работа?");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+const rollback = 10;
+
+const showTypeOf = function (variable) {
+  console.log(variable, typeof variable);
+};
+
+const getRollbackMessage = function (price) {
+  if (price >= 30000) {
+    return "Даем скидку в 10%";
+  } else if (15000 <= price && price < 30000) {
+    return "Даем скидку в 5%";
+  } else if (0 <= price && price < 15000) {
+    return "Скидка не предусмотрена";
+  } else {
+    return "Что то пошло не так";
+  }
+};
+
+const getAllServicePrices = function () {
+  return servicePrice1 + servicePrice2;
+};
+const allServicePrices = getAllServicePrices();
+
+const getFullPrice = function () {
+  return screenPrice + allServicePrices;
+};
+const fullPrice = getFullPrice();
+
+const getTitle = function (string) {
+  const internVar = string.trim().toLowerCase();
+  return internVar[0].toUpperCase() + internVar.substr(1).toLowerCase();
+};
+
+const getServicePercentPrices = function () {
+  return Math.ceil(fullPrice * (1 - rollback / 100));
+};
+const servicePercentPrice = getServicePercentPrices();
+
+const screensSpilt = screens.toLowerCase().split();
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getTitle(title));
+console.log(screensSpilt);
+console.log(screens);
+console.log(getRollbackMessage(fullPrice));
+console.log(servicePercentPrice);
+
+/*
+// 4 урок не принятый
 let title = prompt("Как называется ваш проект?");
 const screens = prompt("Какие типы экранов нужно разработать?");
 const screenPrice = +prompt("Сколько будет стоить данная работа?");
@@ -59,3 +120,5 @@ console.log(screens.split(""));
 console.log(screens);
 console.log(getRollbackMessage(fullPrice));
 console.log(servicePercentPrice);
+
+*/
