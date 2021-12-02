1c4a64874c34b792c0b96ef4237e9d8957120ec2
rus733 <9282716617@mail.ru>
2021-12-02 19:52:25 +0300

    выполнеено  lesson06

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 "use strict";
 
+// lesson06
+//игровой бот.
+//"Загадывание случайного числа от 1 до 100"
+
+let randomNum = 50;
+
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+function inputNumber() {
+  let randomNum = 50;
+  let userNum = prompt("Угадай число от 1 до 100");
+
+  if (userNum === null) {
+    return alert("Игра закончена");
+  } else if (isNumber(userNum)) {
+    userNum = +userNum;
+
+    if (userNum > randomNum) {
+      alert("Загаданное число меньше, введите новый вариант числа");
+    } else if (userNum < randomNum) {
+      alert("Загаданное число больше, введите новый вариант числа");
+    } else if (userNum === randomNum) {
+      alert("Поздравляю, Вы угадали!!!");
+      return userNum;
+    }
+  } else {
+    alert("Введите число!");
+  }
+  return inputNumber();
+}
+
+console.log(inputNumber());
+
+/*
 // lesson05
 
 let title;
@@ -95,3 +131,4 @@ console.log(screensSpilt);
 console.log(screens);
 console.log(getRollbackMessage(fullPrice));
 console.log(servicePercentPrice);
+*/
