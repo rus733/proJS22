a329110a1bf3afc9a5540036c64c215eec3d4fbf
rus733 <9282716617@mail.ru>
2021-12-08 21:27:57 +0300

    изменил названия переменных в соответствием с классом  lesson09

--- js/temp.js
+++ js/temp.js
@@ -1,6 +1,36 @@
 "use strict";
-"use strict";
 
+//задание 9 после первых исправлений
+
+const title = document.getElementsByTagName("h1").title;
+const resetHandlerBtn = document.getElementsByClassName("handler_btn")[0];
+const startHandlerBtn = document.getElementsByClassName("handler_btn")[1];
+const screenBtn = document.querySelector(".screen-btn");
+const otherItemsPercent = document.querySelectorAll(".other-items.percent");
+const otherItemsNumber = document.querySelectorAll(".other-items.number");
+const inpuTypeRange = document.querySelector(".rollback .main-controls__range [type=range]");
+const rangeValue = document.querySelector(".rollback .main-controls__range .range-value ");
+const total = document.getElementsByClassName("total-input")[0];
+const totalCount = document.getElementsByClassName("total-input")[1];
+const totalCountOther = document.getElementsByClassName("total-input")[2];
+const totalFullCount = document.getElementsByClassName("total-input")[3];
+const totalCountRollback = document.getElementsByClassName("total-input")[4];
+let divScreen = document.querySelectorAll(".screen");
+
+console.log(title);
+console.log(resetHandlerBtn);
+console.log(startHandlerBtn);
+console.log(screenBtn);
+console.log(otherItemsPercent);
+console.log(otherItemsNumber);
+console.log(inpuTypeRange);
+console.log(rangeValue);
+console.log(total);
+console.log(totalCount);
+console.log(totalCountOther);
+console.log(totalFullCount);
+console.log(totalCountRollback);
+console.log(divScreen);
 // задание   08 принято
 
 const appData = {
