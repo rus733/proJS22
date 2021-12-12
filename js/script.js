9b501c667b79a933053a7db5110d623cf097ce67
rus733 <9282716617@mail.ru>
2021-12-12 19:35:04 +0300

    изменил имя переменной inpuTypeRange и  rangeValue  на inputRange и  inputRangeValue  lesson09

--- js/script.js
+++ js/script.js
@@ -3,14 +3,15 @@
 
 const title = document.getElementsByTagName('h1').title;
 const buttonPlus = document.querySelector('.screen-btn');
+const otherItemsPercent = document.querySelectorAll('.other-items.percent');
+const otherItemsNumber = document.querySelectorAll('.other-items.number');
+
+const inputRange = document.querySelector('.rollback .main-controls__range [type=range]');
+const inputRangeValue = document.querySelector('.rollback .main-controls__range .range-value ');
 
 const resetHandlerBtn = document.getElementsByClassName('handler_btn')[0];
 const startHandlerBtn = document.getElementsByClassName('handler_btn')[1];
 
-const otherItemsPercent = document.querySelectorAll('.other-items.percent');
-const otherItemsNumber = document.querySelectorAll('.other-items.number');
-const inpuTypeRange = document.querySelector('.rollback .main-controls__range [type=range]');
-const rangeValue = document.querySelector('.rollback .main-controls__range .range-value ');
 const total = document.getElementsByClassName('total-input')[0];
 const totalCount = document.getElementsByClassName('total-input')[1];
 const totalCountOther = document.getElementsByClassName('total-input')[2];
@@ -24,8 +25,8 @@ console.log(startHandlerBtn);
 console.log(buttonPlus);
 console.log(otherItemsPercent);
 console.log(otherItemsNumber);
-console.log(inpuTypeRange);
-console.log(rangeValue);
+console.log(inputRange);
+console.log(inputRangeValue);
 console.log(total);
 console.log(totalCount);
 console.log(totalCountOther);
