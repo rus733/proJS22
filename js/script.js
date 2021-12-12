39cfcbf62791c88e1e4f1a8fc56a5319b0e28908
rus733 <9282716617@mail.ru>
2021-12-12 19:49:49 +0300

    заменил имя переменной divScreen на screens  lesson12

--- js/script.js
+++ js/script.js
@@ -5,23 +5,16 @@ const title = document.getElementsByTagName('h1').title;
 const buttonPlus = document.querySelector('.screen-btn');
 const otherItemsPercent = document.querySelectorAll('.other-items.percent');
 const otherItemsNumber = document.querySelectorAll('.other-items.number');
-
 const inputRange = document.querySelector('.rollback .main-controls__range [type=range]');
 const inputRangeValue = document.querySelector('.rollback .main-controls__range .range-value ');
-
 const startBtn = document.getElementsByClassName('handler_btn')[1];
 const resetBtn = document.getElementsByClassName('handler_btn')[0];
-
 const total = document.getElementsByClassName('total-input')[0];
-
 const totalCount = document.getElementsByClassName('total-input')[1];
-
 const totalCountOther = document.getElementsByClassName('total-input')[2];
-
 const fullTotalCount = document.getElementsByClassName('total-input')[3];
 const totalCountRollback = document.getElementsByClassName('total-input')[4];
-
-let divScreen = document.querySelectorAll('.screen');
+let screens = document.querySelectorAll('.screen');
 
 console.log(title);
 console.log(resetBtn);
@@ -36,7 +29,7 @@ console.log(totalCount);
 console.log(totalCountOther);
 console.log(fullTotalCount);
 console.log(totalCountRollback);
-console.log(divScreen);
+console.log(screens);
 
 const appData = {
   title: '',
