43b1af978a1453d75d297f2b7bbd0ee1d9b2adfb
rus733 <9282716617@mail.ru>
2021-12-13 10:39:10 +0300

    новые методы расчета доп услуг и привязали  допуслуги  к верстке  lesson12

--- js/script.js
+++ js/script.js
@@ -40,11 +40,13 @@ const appData = {
   allServicePrices: 0,
   fullPrice: 0,
   servicePercentPrice: 0,
-  services: {},
+  servicesPercent: {},
+  servicesNumber: {},
   init: function () {
     appData.addTitle();
 
     startBtn.addEventListener('click', appData.start);
+    buttonPlus.addEventListener('click', appData.addScreenBlock);
   },
   addTitle: function () {
     console.log(title.textContent);
@@ -53,6 +55,7 @@ const appData = {
 
   start: function () {
     appData.addScreens();
+    appData.addServices();
     //appData.asking();
     //appData.addPrices();
     //appData.getFullPrice();
@@ -61,15 +64,16 @@ const appData = {
     //appData.logger();
   },
 
-  isNumber: function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-  },
+  //isNumber: function (num) {
+  //return !isNaN(parseFloat(num)) && isFinite(num);
+  //},
 
   addScreens: function () {
+    let screens = document.querySelectorAll('.screen');
     screens.forEach(function (screen, index) {
       const select = screen.querySelector('select');
       const input = screen.querySelector('input');
-      ///console.log(select.value);
+      //console.log(select.value);
       //console.log(input.value);
       const selectName = select.options[select.selectedIndex].textContent;
       appData.screens.push({
@@ -79,6 +83,38 @@ const appData = {
       });
     });
     console.log(appData.screens);
+  },
+
+  addServices: function () {
+    otherItemsPercent.forEach(function (item) {
+      const check = item.querySelector('input[type=checkbox]');
+      const label = item.querySelector('label');
+      const input = item.querySelector('input[type=text]');
+
+      if (check.checked) {
+        appData.servicesPercent[label.textContent] = +input.value;
+      }
+    });
+
+    otherItemsNumber.forEach(function (item) {
+      const check = item.querySelector('input[type=checkbox]');
+      const label = item.querySelector('label');
+      const input = item.querySelector('input[type=text]');
+
+      if (check.checked) {
+        appData.servicesNumber[label.textContent] = +input.value;
+      }
+    });
+    console.log(appData);
+  },
+
+  addScreenBlock: function () {
+    const cloneScreen = screens[0].cloneNode(true);
+
+    //console.log(cloneScreen);
+
+    //screens[screens.lenght - 1].after(cloneScreen);
+    screens[screens.length - 1].after(cloneScreen);
   },
 
   asking: function () {
